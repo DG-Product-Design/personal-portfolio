3dc046b1d5b69d3ead971b7eec36bb614ade23cc
j33p1n <vtdag1@gmail.com>
2022-09-29 19:36:46 -0400

    Update navigation and work section

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import * as AppState from '../AppState';
@@ -6,18 +7,44 @@ import './Nav.scss';
 
 function scrollIntoView(id: string) {
   const yOffset = -78; 
-  const target = document.getElementById(id);;
+  const target = document.getElementById(id);
+	const scrollContainer = document.getElementById('app');
   const y = target ? target.getBoundingClientRect().top + window.pageYOffset + yOffset : 72;
-  window.scrollTo({top: y, behavior: 'smooth'});
+  scrollContainer?.scrollTo({top: y, behavior: 'smooth'});
 }
 
 const Nav = () => {
-  const PDF = process.env.PUBLIC_URL + '/download/David-A-George-Resume.pdf';
+	const [scrollDistance, setScrollDistance] = React.useState<number>(0);
+	const [rootElement, setRootElement] = React.useState<any>();
 
   const screenBelowSm = useRecoilValue(AppState.screenBelowSm);
+	const transparentBg = scrollDistance < 63;
+  const PDF = process.env.PUBLIC_URL + '/download/David-A-George-Resume.pdf';
+
+	React.useEffect(() => {
+		getDistance();
+	});
+
+	React.useEffect(() => {
+		setRootElement(document.getElementById('app'));
+	}, [])
+
+	function getDistance() {
+		if (rootElement) {
+			const scrollTop = rootElement.scrollTop;
+			return setScrollDistance(scrollTop);
+		}
+	};
+
+	React.useEffect(() => {
+		if (rootElement) {
+			rootElement.addEventListener('scroll', getDistance, { passive: true });
+			return () => rootElement.removeEventListener('scroll', getDistance);
+		}
+	});
 
   return (
-    <nav role="navigation" className="w-100 px-2 px-lg-5 py-3">
+    <nav role="navigation" className={classNames(transparentBg ? 'nav--transparent' : 'nav--solid', 'w-100 px-2 px-lg-5 py-3')}>
       <div className="d-flex w-100 container-lg align-items-center justify-content-between">
         <h1 role="link" className="nav__headline link link-blue-300 fs-4 mb-0" onClick={() => scrollIntoView('homepage')} tabIndex={0}>
           { screenBelowSm ? 'DG.' : 'David A. George' }
@@ -26,7 +53,7 @@ const Nav = () => {
           <span role="link" tabIndex={0} className="nav__nav-link link ms-2 ms-sm-3" onClick={() => scrollIntoView('work')}>Work</span>
           <a href={PDF} className="nav__nav-link link ms-2 ms-sm-3" rel='noopener noreferrer' target="_blank" download>Resume</a>
           <a href="mailto:dav.a.george@gmail.com" className="ms-2 ms-sm-3">
-            <button className="dg-btn bg-gradient-green-200 py-2 px-3" onClick={() => {}}>
+            <button className="dg-btn bg-gradient-green-200 py-2 px-3">
               Let's chat
             </button>
           </a>
--- src/sections/WorkSection.tsx
+++ src/sections/WorkSection.tsx
@@ -32,10 +32,10 @@ const WorkSection = () => {
             Property Search
           </React.Fragment>
         }
-        body='Designed and built a lead-gen tool for prospective Real Estate clients to find homes to purchase and schedule tours. Increased tour leads by 3x.'
+        body='I designed and built a lead-gen tool for prospective Real Estate clients to find homes to purchase and schedule tours. Increased tour leads by 3x.'
         button={
           <a href="https://capcenter.com/homes/" rel="noopener noreferrer" target="_blank" className="text-decoration-none">
-            <button className="dg-btn dg-btn--white mb-0 mb-lg-5" onClick={() => {}}>
+            <button className="dg-btn dg-btn--white mb-0 mb-lg-5">
               See live tool
             </button>
           </a>
@@ -51,7 +51,7 @@ const WorkSection = () => {
             Weighted Algorithm<br />Matching Engine
           </React.Fragment>
         }
-        body='Designed and built a tool for social workers in California to match foster parents and children for adoption based on numerous weighted criteria. Won the 2017 Innovation Award from California County Information Services Directors Association (CCISDA).'
+        body='I designed and built a tool for social workers in California to match foster parents and children for adoption based on numerous weighted criteria. Won the 2017 Innovation Award from California County Information Services Directors Association (CCISDA).'
         bannerContent={
 					<React.Fragment>
             <i className='banner__icon fas fa-award w-50 fs-1 text-center mb-3' />
@@ -60,7 +60,7 @@ const WorkSection = () => {
         }
         button={
 					<a href="https://www.youtube.com/watch?v=V3bQb4yMH3k" rel="noopener noreferrer" target="_blank" className="text-decoration-none">
-            <button className="dg-btn dg-btn--white mb-0 mb-lg-5" onClick={() => {}}>
+            <button className="dg-btn dg-btn--white mb-5">
               Watch video by Google
             </button>
           </a>
@@ -73,15 +73,15 @@ const WorkSection = () => {
         color="yellow"
         altImage={
           <div className="work-card__loan-app-image-wrapper--alt d-flex align-items-stretch h-100">
-            <div className="work-card__loan-app-image--alt image--loan-app" />
+            <div className="work-card__loan-app-image--alt" />
           </div>
         }
         imgWrapperClass="col-12 col-lg"
         headline='Fintech Mortgage Loan Application'
-        body='Redesigned the product responsible for the company&apos;s primary source of revenue - the Loan Application. Improved NPS 2+ points and facilitated 500% growth in intake volume.'
+        body='I redesigned the product responsible for the company&apos;s primary source of revenue - the Loan Application. Improved NPS 2+ points and facilitated 500% growth in intake volume.'
         textWrapperClass="work-card__text--padding-top pb-5"
         button={
-          <button className="dg-btn dg-btn--white mb-0 mb-lg-5" onClick={() => {}}>
+          <button className="dg-btn dg-btn--white mb-0 mb-lg-5">
             More Soon
           </button>
         }
@@ -100,9 +100,9 @@ const WorkSection = () => {
           />
         }
         headline='Client Assigner &amp; Management Tool'
-        body='Designed an internal management tool to automatically match clients with consultants based on geolocation, skills and qualifications, and availability. Successfully saved each manager in the program over 30 min per day or 125 hours per year.'
+        body='I designed an internal management tool to automatically match clients with consultants based on geolocation, skills and qualifications, and availability. Successfully saved each manager in the program over 30 min per day or 125 hours per year.'
         button={
-          <button className="dg-btn dg-btn--white mb-0 mb-lg-5" onClick={() => {}}>
+          <button className="dg-btn dg-btn--white mb-5">
             Details in-development
           </button>
         }
@@ -113,10 +113,10 @@ const WorkSection = () => {
         color="blue"
         altImage={
           <div className="work-card__loan-app-image-wrapper--alt d-flex align-items-stretch h-100">
-            <div className="work-card__loan-app-image--alt image--components" />
+            <div className="work-card__component-library-image" />
           </div>
         }
-        imgWrapperClass="col-12 col-lg"
+        imgWrapperClass="col-12 col-lg ps-0 ps-lg-4"
         headline={
 					<React.Fragment>
 						<span className='lh-0'>UI Component Library</span>
@@ -124,7 +124,7 @@ const WorkSection = () => {
 						<span className='fs-5'>React + Storybook + NPM</span>
 					</React.Fragment>
 				}
-        body='Created a UI component library that utilizes Storybook for component documentation and testing. Components are built in React using Typescript and Tailwind. The components are then published as a public NPM package for distribution.'
+        body='I created a UI component library that utilizes Storybook for component documentation and testing. Components are built in React using Typescript and Tailwind. The components are then published as a public NPM package for distribution.'
         textWrapperClass="work-card__text--padding-top pb-5"
         button={
 					<a href='https://github.com/DG-Product-Design/component-library' target='_blank' rel='noopener noreferrer' className='no-underline'>
@@ -134,7 +134,7 @@ const WorkSection = () => {
 					</a>
         }
 				secondaryButton={
-					<div className='d-flex justify-content-center justify-content-lg-start my-3'>
+					<div className='my-3'>
 						<a href='https://www.npmjs.com/package/@j33p1n/component-library' target='_blank' rel='noopener noreferrer' className='no-underline'>
 							<button className='dg-btn dg-btn--white no-underline'>
 								View NPM package
